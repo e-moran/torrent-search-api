126cb542b531d10e447418d13f8b52fde53f0c77
Eoin Moran <eoin.t.moran00@gmail.com>
2019-02-06 23:34:09 +0000

    Tidied up auto-generated NodeJS files and added base server functionality. Began implementation of torrent search api.

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Routes } from "./routes/apiRoutes";
+
+class App {
+    public app: express.Application;
+    public apiRoutes: Routes = new Routes();
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.apiRoutes.routes(this.app);
+    }
+
+    private config() {
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+}
+
+export default new App().app;
