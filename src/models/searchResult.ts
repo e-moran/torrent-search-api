6cb020acd82c232051cb939157207288d3ea70ab
Eoin Moran <eoin.t.moran00@gmail.com>
2019-02-08 00:09:10 +0000

    Finished support for search and retreiving magnet urls

--- src/models/searchResult.ts
+++ src/models/searchResult.ts
@@ -1,6 +1,13 @@
+/*export class SearchResult {
+    constructor( private url: string, private provider: string, public name?: string, public seeds?: number, public leeches?: number, public size?: string) {
+    }
+}*/
+
 export interface SearchResult {
     name: string,
     seeds: number,
     leeches: number,
-    magnetUrl: string
+    size: string,
+    url: string,
+    provider: string
 }
